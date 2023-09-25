2b4e254842053b67a7732e66a08b19367e0b3ded
DASKAL <onurgalib@gmail.com>
2023-09-25 09:35:58 +0300

    25.09.23 г.

--- Zadachi Nachalo/StudentWithBackPack/StudentWithBackPack/Program.cs
+++ Zadachi Nachalo/StudentWithBackPack/StudentWithBackPack/Program.cs
@@ -10,7 +10,28 @@ namespace StudentWithBackPack
     {
         static void Main(string[] args)
         {
-            Student st1 = new Student() {Name="Gancho", FamilyName="Petrov", Age = 15 };
+            List<Student> students = new List<Student>();
+            while (true)
+            {
+                Console.Write("Input student name: ");
+                string studentName = Console.ReadLine();
+                Console.Write("Input student family name: ");
+                string studentFamilyName = Console.ReadLine();
+                Console.Write("Input student age: ");
+                int studentAge = int.Parse(Console.ReadLine());
+                Student student = new Student() { Name = studentName, FamilyName = studentFamilyName, Age = studentAge };
+                //student.BackPack = new BackPack() { Color = "Oranjevo", Price = 19.99m, Volume = 15.6d };
+                //student.BackPack.Color = "Oranjevo";
+                students.Add(student);
+                Console.WriteLine("Input another student? Y/N");
+                string answer = Console.ReadLine().ToUpper();
+                if (answer.StartsWith("N"))
+                {
+                    break;
+                }
+                Console.WriteLine(new string('=', 20));
+            }
+            Student st1 = new Student() { Name = "Gancho", FamilyName = "Petrov", Age = 15 };
             BackPack bp1 = new BackPack() { Volume = 15.6, Color = "Camoflage", Price = 69.75m };
             st1.BackPack = bp1;
             //  st1.Grades = new List<int>();
@@ -26,15 +47,14 @@ namespace StudentWithBackPack
     {
         public double Volume { get; set; }
         public string Color { get; set; }
-        public decimal Price { get;  set; }
+        public decimal Price { get; set; }
     }
     public class Student
     {
         public string Name { get; set; }
         private string familyName;
 
-        public BackPack BackPack { get; set; }
-        public List<int> Grades { get; set; }
+
         public string FamilyName
         {
             get
@@ -45,15 +65,17 @@ namespace StudentWithBackPack
         }
 
         public int Age { get; set; }
+        public BackPack BackPack { get; set; }
+        public List<int> Grades { get; set; }
         public double GetAverageGrade()
         {
-            double result = Grades.Sum()/Grades.Count();
+            double result = Grades.Sum() / Grades.Count();
             return result;
         }
         public void PresentYourself()
         {
-          //  Console.WriteLine($"I am {Name} and I am {Age} ");
-          //  IncreaseAge();
+            //  Console.WriteLine($"I am {Name} and I am {Age} ");
+            //  IncreaseAge();
         }
         private void IncreaseAge()
         {
