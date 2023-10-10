e1bb0c3d0589e88b7e1e6b2fae9195cf447dfd52
Онур Галиб <onurgalib@gmail.com>
2023-10-10 19:28:23 +0300

    Add

--- CandyShop/CandyShop/Program.cs
+++ CandyShop/CandyShop/Program.cs
@@ -2,7 +2,79 @@
 {
     internal class Program
     {
+        public static decimal totalCost { get; private set; }
+
         static void Main(string[] args)
+        {
+            List<Candy> shopItems = new List<Candy>();
+            #region FirstOption
+            FillDataFirstOption(shopItems);
+            #endregion
+            //  FillDataSecondOption(shopItems);
+            Console.WriteLine("List of Products In ourShop:");
+            for (int i = 0; i < shopItems.Count(); i++)
+            {
+                Candy currentCandy = shopItems[i];
+                Console.WriteLine($"{i}. {currentCandy.Name} - {currentCandy.PriceUSD:F2}$.");
+            }
+            while (true)
+            {
+                Console.WriteLine("What do you want to buy? Insert Number - Quantity (1-3)");
+                string input = Console.ReadLine();
+                if (input == "END")
+                {
+                    break;
+                }
+                var choiseofProducts = input.Split("-").Select(x => int.Parse(x)).ToArray();
+                int index = choiseofProducts[0];
+                int ammount = choiseofProducts[1];
+                shopItems[index].SellAmount(ammount);
+                // Console.WriteLine($"you have purchased {shopItems[index].Name}x {ammount}!");
+            }
+            PrintData(shopItems);
+        }
+        private static void PrintData(List<Candy>shopItems)
+        {
+            foreach (var item in shopItems.Where(x => x.CalculateProfit()>0))
+            {
+                totalCost += item.CalculateProfit();
+                Console.WriteLine($"{item.Name}x{item.Sold} [{item.PriceUSD}]={item.CalculateProfit()}");
+            }
+            Console.WriteLine($"Total Money Bill: {totalCost}");
+        }
+        private static void FillDataSecondOption(List<Candy> shopItems)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "END")
+                {
+                    break;
+                }
+                shopItems.Add(new Candy());
+            }
+        }
+
+        private static void FillDataFirstOption(List<Candy> shopItems)
+        {
+            while (true)
+            {
+                //"{Име}; {Калории}; {Тегло}; {Цена}; {Тип на изделието}"
+                string[] inputArr = Console.ReadLine().Split("; ");//Donut; 500; 125; 0.49; Pretzels
+                if (inputArr[0] == "END")
+                {
+                    break;
+                }
+                string name = inputArr[0];
+                int calories = int.Parse(inputArr[1]);
+                double weight = double.Parse(inputArr[2]);
+                decimal price = decimal.Parse(inputArr[3]);
+                FamilyType type = Enum.Parse<FamilyType>(inputArr[4]);
+                shopItems.Add(new Candy(name, calories, weight, price, type));
+            }
+        }
+
+        private static void Demo1()
         {
             var Candy = new Candy();
             string nameofCandy = Candy.Name;
@@ -34,7 +106,7 @@
             }
         }
         public double Weight { get; set; }
-        public decimal PriceUSD 
+        public decimal PriceUSD
         {
             get
             {
@@ -42,9 +114,9 @@
             }
             set
             {
-                if (value<=0)
+                if (value <= 0)
                 {
-                    Console.WriteLine( "Invalid Price for Candy! 0.5$ is not happy!");
+                    Console.WriteLine("Invalid Price for Candy! 0.5$ is not happy!");
                     priceUSD = 5;
                     return;
                 }
@@ -56,10 +128,20 @@
         private FamilyType famType;
 
         public int Amount { get; set; }
-  
+
         public bool IsVeganAcceptable { get; set; }
         public FamilyType FamilyType { get; }
+        public object Sold { get; internal set; }
 
+        public Candy(string input)
+        {
+            var inputArr = input.Split("; ");
+            Name = inputArr[0];
+            Calories = int.Parse(inputArr[1]);
+            Weight = double.Parse(inputArr[2]);
+            PriceUSD = decimal.Parse(inputArr[3]);
+            FamilyType type = Enum.Parse<FamilyType>(inputArr[4]);
+        }
         public Candy(bool isVeganAcceptable = true)
         {
             Amount = 100;
@@ -112,7 +194,7 @@
         }
         public decimal CalculateProfit()
         {
-            return totalSold * PriceUSD;        
+            return totalSold * PriceUSD;
 
         }
 
@@ -122,6 +204,13 @@
     {
         Bonbon,
         Cake,
+        Licorice,
+        Pretzels,
+        Waffle,
+        ShugarMagic,
+        FruitPower,
+        Snacks,
+        Unspecified
 
     }
 }
