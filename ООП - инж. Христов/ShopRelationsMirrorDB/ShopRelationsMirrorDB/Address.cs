c10fe91d01fbc976320b2cd363a93f223af0f430
Онур Галиб <onurgalib@gmail.com>
2023-10-20 09:41:49 +0300

    Add

---
+++ ООП - инж. Христов/ShopRelationsMirrorDB/ShopRelationsMirrorDB/Address.cs
@@ -0,0 +1,27 @@
+﻿namespace ShopRelationsMirrorDB.Models
+{
+    public class Address
+    {
+        public Address()
+        {
+            Shops = new List<Shop>();
+        }
+        public Address(int id, int townName, string streetName, string streetNumber, string description)
+        {
+            Id = id;
+            TownName = townName;
+            StreetName = streetName;
+            StreetNumber = streetNumber;
+            Description = description;
+        }
+
+        public int Id { get; set; }
+        public int TownName { get; set; }
+        public string StreetName { get; set; }
+        public string StreetNumber { get; set; }
+        public string Description { get; set; }
+
+        public virtual IList<Shop> Shops { get; set; }
+
+    }
+}
---
+++ ООП - инж. Христов/ShopRelationsMirrorDB/ShopRelationsMirrorDB/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ShopRelationsMirrorDB.Models
+{
+    public class Shop
+    {
+        public Shop(int id, string name, int startHour, int endHour, int adressId, Address address, int purposeId, Purpose purpose)
+        {
+            Id = id;
+            Name = name;
+            StartHour = startHour;
+            EndHour = endHour;
+            AdressId = adressId;
+            Address = address;
+            PurposeId = purposeId;
+            Purpose = purpose;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int StartHour {  get; set; }
+        public int EndHour { get; set; }
+
+        public int AdressId { get; set; }
+        public virtual Address Address { get; set; }
+
+        public int PurposeId {  get; set; }
+        public virtual Purpose Purpose { get; set; }
+    public virtual IList<PersonShop> ShopPeople { get; set; }
+    }
+    public class Purpose
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public virtual IList<Shop> Shops { get; set; }
+
+    }
+}
