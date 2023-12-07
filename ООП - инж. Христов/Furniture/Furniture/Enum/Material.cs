2c14903e85d6828d4ff558d8ce08ad9e1066366d
Онур Галиб <onurgalib@gmail.com>
2023-12-07 11:08:45 +0200

    Add

---
+++ ООП - инж. Христов/Furniture/Furniture/Enum/Material.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Furnitures
+{
+    public enum Material
+    {
+        Unidentified = 0, PVC = 1, Steel = 2, Aluminum = 3, Wood = 4, Glass = 5
+    }
+}
---
+++ ООП - инж. Христов/Furniture/Furniture/Models/Furniture.cs
@@ -0,0 +1,27 @@
+﻿namespace Furnitures
+{
+    public class Furniture
+    {
+        public int Id { get; set; }
+        public Furniture(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+            Weight = 0;
+            Manufacturer = "StoreShop";
+            Material = Material.Unidentified;
+        }
+
+        public Furniture(string name, decimal price, string manufacturer, Material material) : this(name, price)
+        {
+            Manufacturer = manufacturer;
+            Material = material;
+        }
+
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public double Weight { get; set; }
+        public string Manufacturer { get; set; }
+        public Material Material { get; set; }
+    }
+}
