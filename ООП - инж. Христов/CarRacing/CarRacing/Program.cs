92a5cf06a723b8666579e64c3c31a01d85ed891c
Онур Галиб <onurgalib@gmail.com>
2023-12-11 20:09:05 +0200

    Add

---
+++ ООП - инж. Христов/CarRacing/CarRacing/Program.cs
@@ -0,0 +1,118 @@
+﻿namespace CarRacing
+{
+
+    public interface ICar
+    {
+        string Make { get; }
+        string Model { get; }
+        string VIN { get; }
+        int HorsePower { get; }
+        double FuelAvailable { get; }
+        double FuelConsumptionPerRace { get; }
+
+        void Drive();
+    }
+
+    public interface IRacer
+    {
+        string Username { get; }
+        string RacingBehavior { get; }
+        int DrivingExperience { get; }
+    }
+
+    public class Car : ICar
+    {
+        public string Make { get; }
+        public string Model { get; }
+        public string VIN { get; }
+        public int HorsePower { get; private set; }
+        public double FuelAvailable { get; private set; }
+        public double FuelConsumptionPerRace { get; }
+
+        public Car(string make, string model, string vin, int horsePower, double fuelAvailable, double fuelConsumptionPerRace)
+        {
+            if (string.IsNullOrWhiteSpace(make))
+                throw new ArgumentException("Car make cannot be null or empty.");
+
+            if (string.IsNullOrWhiteSpace(model))
+                throw new ArgumentException("Car model cannot be null or empty.");
+
+            if (string.IsNullOrWhiteSpace(vin) || vin.Length != 17)
+                throw new ArgumentException("Car VIN must be exactly 17 characters long.");
+
+            if (horsePower < 0)
+                throw new ArgumentException("Horse power cannot be below 0.");
+
+            if (fuelConsumptionPerRace < 0)
+                throw new ArgumentException("Fuel consumption cannot be below 0.");
+
+            Make = make;
+            Model = model;
+            VIN = vin;
+            HorsePower = horsePower;
+            FuelAvailable = Math.Max(0, fuelAvailable);
+            FuelConsumptionPerRace = fuelConsumptionPerRace;
+        }
+
+        public virtual void Drive()
+        {
+            FuelAvailable = Math.Max(0, FuelAvailable - FuelConsumptionPerRace);
+            if (this is TunedCar tunedCar)
+            {
+                double wearPercentage = 0.03; // 3% wear
+                tunedCar.ReduceHorsePowerByPercentage(wearPercentage);
+            }
+        }
+    }
+
+    public class SuperCar : Car
+    {
+        public SuperCar(string make, string model, string vin, int horsePower)
+            : base(make, model, vin, horsePower, 80, 10)
+        {
+        }
+    }
+
+    public class TunedCar : Car
+    {
+        public TunedCar(string make, string model, string vin, int horsePower)
+            : base(make, model, vin, horsePower, 65, 7.5)
+        {
+        }
+
+        public void ReduceHorsePowerByPercentage(double percentage)
+        {
+            ReduceHorsePowerByPercentage(percentage, HorsePower);
+        }
+
+        public void ReduceHorsePowerByPercentage(double percentage, int horsePower)
+        {
+            int reduction = (int)Math.Round(HorsePower * percentage);
+            horsePower = Math.Max(0, HorsePower - reduction);
+        }
+    }
+
+    public class Racer : IRacer
+    {
+        public string Username { get; }
+        public string RacingBehavior { get; }
+        public int DrivingExperience { get; }
+
+        public Racer(string username, string racingBehavior, int drivingExperience)
+        {
+            if (string.IsNullOrWhiteSpace(username))
+                throw new ArgumentException("Username cannot be null or empty.");
+
+            if (string.IsNullOrWhiteSpace(racingBehavior))
+                throw new ArgumentException("Racing behavior cannot be null or empty.");
+
+            if (drivingExperience < 0 || drivingExperience > 100)
+                throw new ArgumentException("Racer driving experience must be between 0 and 100.");
+
+            Username = username;
+            RacingBehavior = racingBehavior;
+            DrivingExperience = drivingExperience;
+        }
+    }
+
+}
---
+++ ООП - инж. Христов/RegisterAutomobilies/RegisterAutomobilies/Program.cs
@@ -0,0 +1,259 @@
+﻿namespace RegisterAutomobilies
+{
+
+
+
+    public enum Manufacturer
+    {
+        Ford,
+        Mercedes,
+        BMW,
+        Audi,
+        Tesla
+    }
+
+    public class Battery
+    {
+        public int Capacity { get; set; }
+    }
+
+    public abstract class CarModel
+    {
+        protected CarModel(string model, Manufacturer manufacturer, int startYearOfModel, double maxSpeedKmPh, double weight, double loadCapacity)
+        {
+            Model = model;
+            Manufacturer = manufacturer;
+            StartYearOfModel = startYearOfModel;
+            MaxSpeedKmPh = maxSpeedKmPh;
+            Weight = weight;
+            LoadCapacity = loadCapacity;
+        }
+
+        public string Model { get; }
+        public Manufacturer Manufacturer { get; }
+        public int StartYearOfModel { get; }
+        public double MaxSpeedKmPh { get; set; }
+        public double Weight { get; }
+        public double LoadCapacity { get; }
+
+        protected double GetHoursForTravel(double distance)
+        {
+            return distance / MaxSpeedKmPh;
+        }
+    }
+
+    public interface ITravelable
+    {
+        double MaxTravelDistance { get; }
+        void Refuel();
+        TripInfo Travel(double distance);
+    }
+
+    public class TripInfo
+    {
+        public double Distance { get; }
+        public double Hours { get; }
+
+        public TripInfo(double distance, double hours)
+        {
+            Distance = distance;
+            Hours = hours;
+        }
+    }
+
+    public class ElectricCarData : CarModel, ITravelable
+    {
+        public Battery Battery { get; set; }
+        public double TravelDistanceKoef { get; }
+        public override double MaxTravelDistance => Battery.Capacity * TravelDistanceKoef;
+
+        public ElectricCarData(string model, Manufacturer manufacturer, int startYearOfModel, double maxSpeedKmPh, double weight, double loadCapacity, Battery battery, double travelDistanceKoef)
+            : base(model, manufacturer, startYearOfModel, maxSpeedKmPh, weight, loadCapacity)
+        {
+            Battery = battery;
+            TravelDistanceKoef = travelDistanceKoef;
+        }
+
+        public void Refuel()
+        {
+            Console.WriteLine("Battery recharged.");
+        }
+
+        public TripInfo Travel(double distance)
+        {
+            if (distance > MaxTravelDistance)
+            {
+                throw new ApplicationException("ImpossibleTravel!");
+            }
+
+            double hours = GetHoursForTravel(distance);
+
+            if (Battery.Capacity < 100)
+            {
+                Console.WriteLine("Recharge needed...");
+            }
+
+            return new TripInfo(distance, hours);
+        }
+    }
+    public class FuelCarData : CarModel, ITravelable
+    {
+        public Tank Tank { get; set; }
+        public double MaxTravelDistanceKoef { get; }
+        public override double MaxTravelDistance => Tank.Capacity * MaxTravelDistanceKoef;
+
+        public FuelCarData(string model, Manufacturer manufacturer, int startYearOfModel, double maxSpeedKmPh, double weight, double loadCapacity, Tank tank, double maxTravelDistanceKoef)
+            : base(model, manufacturer, startYearOfModel, maxSpeedKmPh, weight, loadCapacity)
+        {
+            Tank = tank;
+            MaxTravelDistanceKoef = maxTravelDistanceKoef;
+        }
+
+        public void Refuel()
+        {
+            Console.WriteLine("Tank refueled.");
+        }
+        public TripInfo Travel(double distance)
+        {
+            if (distance > MaxTravelDistance)
+            {
+                throw new ApplicationException("ImpossibleTravel!");
+            }
+
+            double hours = GetHoursForTravel(distance);
+
+            if (Tank.Capacity < 100)
+            {
+                Console.WriteLine("Refuel needed...");
+            }
+
+            return new TripInfo(distance, hours);
+        }
+    }
+
+    public class Tank
+    {
+        public int Capacity { get; set; }
+    }
+
+    public interface ITravelable
+    {
+        double MaxTravelDistance { get; }
+        void Refuel();
+        TripInfo Travel(double distance);
+    }
+    public enum FuelType
+    {
+        Benzine,
+        Gas,
+        Diesel,
+        Metan,
+        Coal,
+        Wood
+    }
+    public class Tank
+    {
+        public double MaxCapacity { get; set; }
+        public double RemainingCapacity { get; }
+        public string Type { get; }
+        public Tank()
+        {
+            MaxCapacity = 50;
+            RemainingCapacity = 50;
+            Type = "Benzine";
+        }
+        public Tank(double maxCapacity, string type = "Benzine")
+        {
+            MaxCapacity = maxCapacity;
+            RemainingCapacity = maxCapacity;
+            Type = type;
+        }
+        public void Refuel()
+        {
+            RemainingCapacity = MaxCapacity;
+        }
+    }
+    public class Battery
+    {
+        public enum TechnologyType
+        {
+            LiIon,
+            NiOx,
+            Gel,
+            LedAcid
+        }
+        public TechnologyType BatteryTechnology { get; }
+        private int rechargeCyclesMax;
+        private int cyclesReached;
+        public int MaxCapacity { get; }
+        public double Capacity { get; private set; }
+        public double Health => (double)(rechargeCyclesMax - cyclesReached) / rechargeCyclesMax;
+        public Battery(TechnologyType technologyType, int rechargeCyclesMax, double maxCapacity)
+        {
+            BatteryTechnology = technologyType;
+            this.rechargeCyclesMax = rechargeCyclesMax;
+            MaxCapacity = maxCapacity;
+            Capacity = maxCapacity;
+        }
+        public void Recharge()
+        {
+            if (cyclesReached >= rechargeCyclesMax)
+            {
+                throw new BatteryFailureException("Battery is dead, replacement needed");
+            }
+
+            Capacity = MaxCapacity;
+            cyclesReached++;
+        }
+    }
+    public interface ITravelable
+    {
+        double MaxTravelDistance { get; }
+        void Refuel();
+        TripInfo Travel(double distance);
+    }
+    public class TripInfo
+    {
+        public double TravelTime { get; set; }
+        public double Distance { get; }
+        public string ModelCar { get; }
+        public TripInfo(string modelCar, double travelTime, double distance)
+        {
+            ModelCar = modelCar;
+            TravelTime = travelTime;
+            Distance = distance;
+        }
+        public override string ToString()
+        {
+            return $"{ModelCar} traveled {Distance} km, for {TravelTime} hours";
+        }
+    }
+    public class BatteryFailureException : Exception
+    {
+        public BatteryFailureException(string message) : base(message) { }
+    }
+    public enum Manufacturer
+    {
+        Ford,
+        Mercedes,
+        BMW,
+        Audi,
+        Tesla
+    }
+    public enum TechnologyType
+    {
+        LiIon,
+        NiOx,
+        Gel,
+        LedAcid
+    }
+    public enum FuelType
+    {
+        Benzine,
+        Gas,
+        Diesel,
+        Metan,
+        Coal,
+        Wood
+    }
+} 
