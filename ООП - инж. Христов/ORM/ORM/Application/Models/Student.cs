fdebb1c4e4fb78c25555d4d8640b397608d60f88
Онур Галиб <onurgalib@gmail.com>
2024-01-19 08:56:29 +0200

    Add

---
+++ ООП - инж. Христов/ORM/ORM/Application/Models/Student.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public Student()
+        {
+
+        }
+        public Student(string name, int grade, char gradeLetter) : this()
+        {
+            Name = name;
+            Grade = grade;
+            GradeLetter = gradeLetter;
+        }
+
+        public string Name { get; set; }
+        public int Grade { get; set; }
+        public char GradeLetter { get; set; }
+
+        [MinLength(16),Phone]
+        public string GSM { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        //FK
+        public int RoomId { get; set; }
+        public virtual Room Room { get; set; }
+
+
+    }
+}
