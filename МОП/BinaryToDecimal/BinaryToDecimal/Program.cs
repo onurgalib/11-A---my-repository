14781ab648d65fc9d1e250f0453d5b728f97a7e2
Онур Галиб <onurgalib@gmail.com>
2024-02-08 15:16:30 +0200

    Add

---
+++ МОП/BinaryToDecimal/BinaryToDecimal/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static string DecimalToBinary(int decimalNumber)
+    {
+        if (decimalNumber == 0)
+        {
+            return "0";
+        }
+
+        string binary = "";
+        while (decimalNumber > 0)
+        {
+            binary = (decimalNumber % 2) + binary;
+            decimalNumber /= 2;
+        }
+        return binary;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Въведете число в десетична бройна система:");
+        if (!int.TryParse(Console.ReadLine(), out int decimalInput) || decimalInput < 0)
+        {
+            Console.WriteLine("Моля въведете положително число.");
+            return;
+        }
+
+        if (decimalInput > 2147483647) // максималната стойност на int32
+        {
+            Console.WriteLine("Въведеното число е по-голямо от максималната стойност на int32.");
+            return;
+        }
+
+        string binaryResult = DecimalToBinary(decimalInput);
+        Console.WriteLine($"Резултатът в двоична бройна система е: {binaryResult}");
+    }
+}
---
+++ МОП/DecimalToHexadecimal/DecimalToHexadecimal/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static string DecimalToHexadecimal(int decimalNumber)
+    {
+        if (decimalNumber == 0)
+        {
+            return "0";
+        }
+
+        string hexadecimal = "";
+        while (decimalNumber > 0)
+        {
+            int remainder = decimalNumber % 16;
+            hexadecimal = (remainder < 10 ? (char)(remainder + '0') : (char)(remainder - 10 + 'A')) + hexadecimal;
+            decimalNumber /= 16;
+        }
+        return hexadecimal;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Въведете число в десетична бройна система:");
+        if (!int.TryParse(Console.ReadLine(), out int decimalInput))
+        {
+            Console.WriteLine("Моля въведете валидно цяло число.");
+            return;
+        }
+
+        string hexadecimalResult = DecimalToHexadecimal(decimalInput);
+        Console.WriteLine($"Резултатът в шестнадесетична бройна система е: {hexadecimalResult}");
+    }
+}
