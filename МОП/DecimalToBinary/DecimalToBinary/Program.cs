b3e29c52be1bbb555f1488d823873fc002cf4c3f
Онур Галиб <onurgalib@gmail.com>
2024-02-08 15:14:16 +0200

    Add

---
+++ МОП/DecimalToBinary/DecimalToBinary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Въведете число в десетична бройна система:");
+        int input = Convert.ToInt32(Console.ReadLine());
+
+        string binary = DecimalToBinary(input);
+
+        Console.WriteLine($"Числото {input} в двоична бройна система е: {binary}");
+    }
+
+    static string DecimalToBinary(int number)
+    {
+        if (number == 0)
+            return "0";
+
+        string result = "";
+
+        while (number > 0)
+        {
+            int remainder = number % 2;
+            result = remainder + result;
+            number /= 2;
+        }
+
+        return result;
+    }
+}
